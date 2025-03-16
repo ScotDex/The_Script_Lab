dd7c530da4deffb5d7161463da829af0e99749ee
ScotDex <themadlyscientific@gmail.com>
2025-03-16 17:03:50 +0000

    update

--- API/eve.ps1
+++ API/eve.ps1
@@ -13,15 +13,21 @@
 # The code is based on the example provided in the EVE Online documentation: https://esi.evetech.net/ui/?version=latest#/Character/get_characters_character_id
 $clientID="539efdfedabe4ca19575d01b6ae5ba8e"
 $clientSecret="xBMQMOOZVxfQI8RQPSjKEKXvqsdJKybv8UceDZjY"
-$redirectUri="https://cce5-31-111-83-157.ngrok-free.app"
-$scopes = "publicData"
+# $credentials = "{$username}:{$password}"
+# $credentialBytes = [System.Text.Encoding]::ASCII.GetBytes($credentials)
+# $base64AuthInfo = [Convert]::ToBase64String(($credentialBytes))
+# $basicAuthHeader = "Basic " + $base64AuthInfo
+$redirectUri="https://c6ce-146-70-204-179.ngrok-free.app/callback"
+$scopes = "publicData esi-assets.read_assets.v1"
 $state = [Guid]::NewGuid().ToString() # Generate a random state value
-$tokenUri = "https://login.eveonline.com/oauth/token"
-$authUrl = "https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=$redirectUri&client_id=$clientID&scope=$scopes&state=$state"
+$encodedScope = [System.Web.HttpUtility]::UrlEncode($scopes)
+$tokenUri = "https://login.eveonline.com/v2/oauth/token"
+$authUrl = "https://login.eveonline.com/v2/oauth/authorize?response_type=code&redirect_uri=$redirectUri&client_id=$clientID&scope=$encodedScope&state=$state"
+$assetsUrl = "https://esi.evetech.net/latest/characters/95282689/assets/"
+$encodedAssets = [System.Web.HttpUtility]::UrlEncode($assetsUrl)
 Write-Host "Please visit this URL to authenticate: $authUrl"
 
-$authorizationCode = "8r0Q7Owge5V4VHzNpm1BaGfmyaS47mRXVTDkomRsi-QzZKC1bramNhFMW1Lo-qpA"
-
+$authorizationCode = "ZaazFJl76kSX_87Z0GQJ1A"
 
 $body = @{
     grant_type    = "authorization_code"
@@ -30,72 +36,21 @@ $body = @{
     client_id     = $clientID
     client_secret = $clientSecret
 }
-$response = Invoke-RestMethod -Uri $tokenUri -Method Post -Body $body -ContentType "application/x-www-form-urlencoded"
-$response
+$response = Invoke-RestMethod -Uri "$tokenUri" -Method Post -ContentType "application/x-www-form-urlencoded" -Body $body
 
 $accessToken = $response.access_token
 Write-Host "Access Token: $accessToken"
-
-$uri = "https://esi.evetech.net/latest/characters/95282689/"
-$characterInfo = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-
-$characterInfo
-
-$accessToken = $response.access_token
 
 $headers = @{
     Authorization = "Bearer $accessToken"
     "Content-Type"="application/json"
 }
 
+# The code below is used to get the character information for a specific character
 
 $uri = "https://esi.evetech.net/latest/characters/95282689/"
 $characterInfo = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-
 $characterInfo
 
-# Assuming $listener is your active HttpListener object
-
-
-# $urlbase = "https://api.example.com" # The base url of the api
-# $uri = $urlbase + "/endpoint" # The endpoint of the api
-
-# $headers = @{ 
-#     Authorization=$basicAuthHeader
-#     "Content-Type"="application/json"
-# }
-
-# Invoke-RestMethod -Uri "$urlbase/" -Headers $headers -Method Get
-
-
-# Partial code for oauth2 authentication
-# $uri = "https://api.example.com"
-
-#$body = @{
-#"client_id"="value1"
-#"client_secret"="value2"
-#} | ConvertTo-Json
-
-#$headers = @{
-#    Authorization = "bearer $accesstoken"
-#    "Content-Type"="application/json"
-#}
-#Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-
-
-# param (
-#     [Parameter(Mandatory=$true)]
-#     [string]$TargetURL,
-
-#     [Parameter(Mandatory=$true)]
-#     [hashtable]$Headers,
-
-#     [Parameter(Mandatory=$true)]
-#     [string]$Payload
-# )
-
-# $Body = @{ "query" = $Payload } | ConvertTo-Json
-# Write-Host "[*] Sending large payload to $TargetURL..."
-# Invoke-WebRequest -Uri $TargetURL -Method POST -Headers $Headers -Body $Body -TimeoutSec 10
-
-
+$assetResponse = Invoke-RestMethod -Uri "$encodedAssets" -Headers $headers -Method Get
+$assetResponse
--- localhost/server.js
+++ localhost/server.js
@@ -4,7 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
+app.get('/callback', (req, res) => {
+  res.send('This is the callback route!');
+});
 app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080');
 });
